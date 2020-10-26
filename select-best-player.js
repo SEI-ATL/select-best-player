f770d7d8cf8f5486588d453e384a8fc60f62faf1
Rome Bell <romebell@me.com>
2020-10-26 05:51:12 -0700

    [select-best-player] Add array of teams

---
+++ select-best-player.js
@@ -0,0 +1,14 @@
+nba = [
+  {
+    lebron: 'LeBron James',
+    ad: 'Anthony Davis'
+  },
+  {
+    dame: 'Damien Lillard',
+    cj: 'CJ McCollough'
+  },
+  {
+    harden: 'James Harden',
+    lin: 'Jeremy Linn'
+  }
+];
