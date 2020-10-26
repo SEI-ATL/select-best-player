2306a8375010f046011e53e9d867e0ac8247de37
Rome Bell <romebell5@gmail.com>
2020-10-26 05:56:49 -0700

    [select-best-player] Add function

--- select-best-player.js
+++ select-best-player.js
@@ -12,3 +12,33 @@ nba = [
     lin: 'Jeremy Linn'
   }
 ];
+
+function selectBestPlayer(sport) {
+  const bestPlayers = [];
+  let anthnoyDavis = 'ad';
+
+  for (let i = 0; i < sport.length; i++) {
+    let team = sport[i];
+    console.log(`The team is : ${team}`);
+
+    if (team.harden) {
+      console.log('James Harden was selected');
+      bestPlayers.push(team.harden);
+    } else if (team['lebron']) {
+      console.log('Lebron James was selected');
+      bestPlayers.push(team.lebron);
+    } else if (team.dame) {
+      console.log('Damien Lillard was selected');
+      bestPlayers.push(team['dame']);
+    }
+    
+    if (team[anthnoyDavis]) {
+      console.log('Anthony Davis was selected');
+      bestPlayers.push(team.ad);
+    }
+  }
+
+  return bestPlayers;
+}
+
+console.log(selectBestPlayer(nba));
